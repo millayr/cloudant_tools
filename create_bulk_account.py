e85e8692c2a403a210f1b50afec462fc8a5986ae
Ryan Millay <ryanmillay@Ryans-MacBook-Pro-2.local>
2014-11-17 10:46:07 -0500

    Useful tools for testing

---
+++ create_bulk_account.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import multiprocessing.dummy as multiprocessing
+import uuid
+
+num_dbs = 20
+num_docs = 5000
+account = 'millayr-bulk'
+pwd = 'SJ_class07'
+num_threads = 5
+s = requests.Session()
+
+def create_dbs(docs):
+	for i in range(num_dbs):
+		db = uuid.uuid1()
+		r = s.put('https://{0}.cloudant.com/db-{1}'.format(account, db), auth=(account, pwd)).json()
+		r = s.post('https://{0}.cloudant.com/db-{1}/_bulk_docs'.format(account, db), 
+			headers={'Content-Type': 'application/json'}, auth=(account, pwd), data=json.dumps(docs)).json()
+		print 'POSTed docs to db-{0}...'.format(db)
+
+
+def main():
+	doc = {'docs': []}
+
+	for i in range(num_docs):
+		doc['docs'].append({'Years': 18, 'Name': 'Angel', 'Job': 'Clerk', 'Dept': 93, 'Salary': 53055})
+
+	print json.dumps(doc, indent=4)
+	threads = []
+	for i in range(num_threads):
+		t = multiprocessing.Process(target=create_dbs, args=(doc,))
+		threads.append(t)
+		t.start()
+
+	for t in threads:
+		t.join()
+
+if __name__ == "__main__":
+	main()
---
+++ delete_dbs.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import multiprocessing.dummy as multiprocessing
+import uuid
+
+account = 'millayr-test'
+pwd = 'SJ_class07'
+num_threads = 10
+
+s = requests.Session()
+
+
+def slice_array(array, size):
+	if size < 1:
+		size = 1
+	return [array[i:i + size] for i in range(0, len(array), size)]
+
+
+def delete_dbs(dbs):
+	for db in dbs:
+		r = s.delete('https://{0}.cloudant.com/{1}'.format(account, db), auth=(account, pwd)).json()
+		print 'Deleted {0}...'.format(db)
+
+
+def main():
+	dbs = s.get('https://{0}.cloudant.com/_all_dbs'.format(account), auth=(account, pwd)).json()
+	if '_replicator' in dbs:
+		dbs.remove('_replicator')
+
+	range_size = len(dbs) // num_threads
+	db_chunks = slice_array(dbs, range_size)
+
+	threads = []
+	for chunk in db_chunks:
+		t = multiprocessing.Process(target=delete_dbs, args=(chunk,))
+		threads.append(t)
+		t.start()
+
+	for t in threads:
+		t.join()
+
+if __name__ == "__main__":
+	main()
